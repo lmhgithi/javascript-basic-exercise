8fcab7b5252db9c47c465ed67568007475f47cf1
lmhgithi <466503176@qq.com>
2020-07-20 17:21:17 +0800

    [liminghao] feat: add numberFormatter

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -7,6 +7,9 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  let fixedNum = number.toFixed(2);
+  if (option && option.currency) {
+    fixedNum = `$ ${fixedNum}`;
+  }
+  return fixedNum;
 }
