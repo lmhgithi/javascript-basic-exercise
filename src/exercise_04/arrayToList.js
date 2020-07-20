9e610a77a88ff45e96971be80e75578d4eeea9c3
lmhgithi <466503176@qq.com>
2020-07-20 18:24:32 +0800

    [liminghao] feat: add arrayToList

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -6,5 +6,25 @@ export default function arrayToList(array) {
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  if (array === undefined || array === null) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+
+  const head = {};
+  // eslint-disable-next-line prefer-destructuring
+  head.value = array[0];
+  head.next = null;
+  let pre; let cur;
+  pre = head;
+  for (let i = 1; i < array.length; i += 1) {
+    cur = {};
+    cur.value = array[i];
+    pre.next = cur;
+    pre = cur;
+    cur.next = null;
+  }
+  return head;
 }
