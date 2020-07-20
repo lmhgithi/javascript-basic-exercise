a834a6d66a36db2e4d6129b7a32c29b541492550
lmhgithi <466503176@qq.com>
2020-07-20 19:20:04 +0800

    [liminghao] feat: add flattenArray

--- src/exercise_05/flattenArray.js
+++ src/exercise_05/flattenArray.js
@@ -5,6 +5,17 @@ export default function flattenArray(array) {
   //
   // * Please implement this function and pass all the tests in flatten_array_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (!array) {
+    throw new Error('Flatten undefined or null array');
+  }
+  const ret = [];
+  array.forEach((val) => {
+    if (val instanceof Array) {
+      // console.log(...val);
+      ret.push(...val);
+    } else {
+      ret.push(val);
+    }
+  });
+  return ret;
 }
