d33cfb60af9d312e94878123559ecff5b1e3ac75
lmhgithi <466503176@qq.com>
2020-07-20 17:43:32 +0800

    [liminghao] feat: add characterCounter

--- src/exercise_02/characterCounter.js
+++ src/exercise_02/characterCounter.js
@@ -5,6 +5,15 @@ export default function countCharacter(string, prediction) {
   //
   // * Please implement this function and pass all the tests in character_counter_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (!string) {
+    return 0;
+  }
+  if (!prediction) {
+    return string.length;
+  }
+  let n = 0;
+  for (let i = 0; i < string.length; i += 1) {
+    if (prediction(string[i])) { n += 1; }
+  }
+  return n;
 }
