2e212f4b56d994b41b7c090accf0171807af6103
lmhgithi <466503176@qq.com>
2020-07-20 21:20:50 +0800

    [liminghao] feat: add waitForAll

--- src/exercise_07/waitForAll.js
+++ src/exercise_07/waitForAll.js
@@ -9,5 +9,28 @@ export default function waitForAll(...promises) {
   // * Please implement this function and pass all the tests in wait_for_all_spec.js.
   // * Please do NOT modify the signature of the function.
 
-  throw new Error('Please delete this line and implement the function');
+  promises.forEach((p) => {
+    if (!(p instanceof Promise)) {
+      throw new Error('Not all elements are promises.');
+    }
+  });
+  return new Promise((resolve, reject) => {
+    let count = 0;
+    for (let i = 0; i < promises.length; i += 1) {
+      // eslint-disable-next-line no-loop-func
+      promises[i].then(() => {
+        count += 1;
+        if (count === promises.length) {
+          resolve();
+        }
+        if (i === promises.length - 1 && count !== promises.length) {
+          reject();
+        }
+      // eslint-disable-next-line no-loop-func
+      }).catch(() => {
+        count -= 1;
+      // eslint-disable-next-line no-loop-func
+      });
+    }
+  });
 }
