49cd890bed32bbc839bdaa9bab8d6c3b9fcefbee
lmhgithi <466503176@qq.com>
2020-07-20 19:50:45 +0800

    [liminghao] feat: add vector

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -11,6 +11,34 @@ export default class Vector {
   // * Please implement the class and pass all the tests in vector_spec.js.
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
   // class.
+  constructor(x1, y1) {
+    this._x = x1;
+    this._y = y1;
+  }
 
-  // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  static plus(v1, v2) {
+    const resOfPlus = {};
+    resOfPlus.x = v1._x + v2._x;
+    resOfPlus.y = v1._y + v2._y;
+    return resOfPlus;
+  }
+
+  static minus(v1, v2) {
+    const resOfMinus = {};
+    resOfMinus.x = v1._x - v2._x;
+    resOfMinus.y = v1._y - v2._y;
+    return resOfMinus;
+  }
+
+  distance() {
+    return Math.sqrt(this._x * this._x + this._y * this._y);
+  }
 }
